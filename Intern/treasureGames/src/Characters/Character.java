a42e5bf97614d3acc059dc82fd1d91a8cb162b22
Gracelin Jonax Edward <jonax.gracelin@divum.in>
2024-01-05 18:23:59 +0530

    games

---
+++ Intern/treasureGames/src/Characters/Character.java
@@ -0,0 +1,9 @@
+package Characters;
+
+import java.awt.image.BufferedImage;
+
+public class Character {
+    public int x,y;
+    public int speed;
+    public BufferedImage up;
+}
---
+++ Intern/treasureGames/src/Characters/Player.java
@@ -0,0 +1,36 @@
+package Characters;
+
+import Main.GamePanel;
+import Main.KeyHandler;
+
+import java.awt.*;
+
+public class Player extends Character{
+    GamePanel gamePanel;
+    KeyHandler keyHandler;
+    public Player(GamePanel gamePanel,KeyHandler keyHandler){
+    this.gamePanel=gamePanel;
+    this.keyHandler=keyHandler;
+    setDefaultValues();
+    }
+    public void setDefaultValues(){
+        x=100;
+        y=100;
+        speed=4;
+    }
+    public void update(){
+        if(keyHandler.up==true)
+            y-=speed;
+        else if(keyHandler.down==true)
+            y+=speed;
+        else if(keyHandler.left==true)
+            x-=speed;
+        else if (keyHandler.right==true)
+            x+=speed;
+    }
+
+    public void draw(Graphics2D g2){
+        g2.setColor(Color.WHITE);
+        g2.fillRect(x,y,gamePanel.titleSize,gamePanel.titleSize);
+    }
+}
