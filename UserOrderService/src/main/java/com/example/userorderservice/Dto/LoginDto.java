0fa90ced87c767cc57444ad8438981646ea45c3b
Gracelin Jonax Edward <jonax.gracelin@divum.in>
2024-02-27 09:53:29 +0530

    error in push

--- UserOrderService/src/main/java/com/example/userorderservice/Dto/LoginDto.java
+++
@@ -1,9 +0,0 @@
-package com.example.userorderservice.Dto;
-
-import lombok.Data;
-
-@Data
-public class LoginDto {
-    private String email;
-    private String password;
-}
--- UserOrderService/src/main/java/com/example/userorderservice/repository/service/UserDetailsRepositoryService.java
+++
@@ -1,9 +0,0 @@
-package com.example.userorderservice.repository.service;
-
-import com.example.userorderservice.model.UserDetails;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface UserDetailsRepositoryService {
-    UserDetails findByEmail(String email);
-}
